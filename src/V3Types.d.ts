494b90f09cd8d40e22715860bc520d05693a4c5d
Preston Riley <123032424+Preston-Riley@users.noreply.github.com>
2024-04-05 15:38:33 -0700

    up

---
+++ src/V3Types.d.ts
@@ -0,0 +1,39 @@
+import type { BareHeaders } from './BareTypes.js';
+
+export interface BareResponseHeaders {
+	status: number;
+	statusText: string;
+	headers: BareHeaders;
+}
+
+export interface SocketClientToServer {
+	type: 'connect';
+	/**
+	 * Remote to connect to
+	 */
+	remote: string;
+	/**
+	 * An array of protocols to attempt to connect to.
+	 */
+	protocols: string[];
+	/**
+	 * Headers to send to the remote. Usually Cookie, Origin, and User-Agent.
+	 */
+	headers: BareHeaders;
+	/**
+	 * Forwards to forward from the WebSocket handshake (eg. User-Agent)
+	 */
+	forwardHeaders: string[];
+}
+
+export interface SocketServerToClient {
+	type: 'open';
+	/**
+	 * The protocl that the remote chose.
+	 */
+	protocol: string;
+	/**
+	 * A list of cookies that correspond to the remote's set-cookies
+	 */
+	setCookies: string[];
+}
